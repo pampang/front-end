d76ee4fba80ab19508d36aead46e4ace70dbba05
pampang <fspwz@sina.com>
2015-10-06 22:46:33 +0800

    数组打乱#v1.1

--- js-tools/数组打乱.js
+++ js-tools/数组打乱.js
@@ -1,4 +1,7 @@
 //数组打乱
+
+//方法1：给定一个随机的index，如果仍在arr中存在，则push如copy数组中；否则，继续循环。
+//坏处：如果到了后期，寻找数组中仍存在的index，会非常困难，耗费极多资源。
 function shuffle1(arr){
 	var copy = [], 
 		n = arr.length, 
@@ -14,6 +17,8 @@ function shuffle1(arr){
 	}
 }
 
+//方法2：用splice取得一个随机提取的数组元素。这样原数组的结构不会混乱，每次随机得出的index都能指定数组中的值。
+//坏处：每取得一次元素，数组都需要重新排序。
 function shuffle2(arr){
 	var copy = [],
 		n = arr.length,
